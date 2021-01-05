0fa3b7c71d36b7ef1ccfb67108a3992ddd8eb65d
TriambakGoyal <triambak.goyal@science.christuniversity.in>
2021-01-06 04:26:14 +0530

    API CALLS WORKING UI LEFT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hacker-News';
+  hnews:any=[];
+  no_hnews:number;
+  hnews_info:any=[];
+  constructor(private http: HttpClient){
+    this.http.get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty").
+    subscribe(response =>{
+      this.hnews=response;
+      this.no_hnews=this.hnews.length;
+      console.log(this.hnews.length)
+
+    for(let i=0; i<this.no_hnews;i++){
+      this.http.get("https://hacker-news.firebaseio.com/v0/item/"+this.hnews[i]+".json?print=pretty").
+      subscribe(data => {
+          this.hnews_info.push(data);
+      });
+    }
+
+    });
+
+  }
+
+  ngOnInit(){
+
+    // this.http.get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty").
+    // subscribe(response =>{
+    //   this.hnews=response;
+    //   this.no_hnews=this.hnews.length;
+    //   console.log(this.hnews.length)
+
+    // for(let i=0; i<this.no_hnews;i++){
+    //   this.http.get("https://hacker-news.firebaseio.com/v0/item/"+this.hnews[i]+".json?print=pretty").
+    //   subscribe(data => {
+    //       this.hnews_info.push(data);
+    //   });
+    // }
+
+    // });
+
+
+
+  }
 }
