a5be892a09264e1185f3622b94920c654f9fc800
TriambakGoyal <triambak.goyal@science.christuniversity.in>
2021-01-06 16:03:00 +0530

    Working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,22 @@ export class AppComponent {
   hnews:any=[];
   no_hnews:number;
   hnews_info:any=[];
+
+
   constructor(private http: HttpClient){
+
+    // Get request to get the list of IDs of all the top stories
+
     this.http.get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty").
     subscribe(response =>{
       this.hnews=response;
       this.no_hnews=this.hnews.length;
       console.log(this.hnews.length)
 
-    for(let i=0; i<this.no_hnews;i++){
+      //Fetching the json object with respect to each ID recieved from the top-stories API
+
+    for(let i=0; i<this.no_hnews;i++)
+    {
       this.http.get("https://hacker-news.firebaseio.com/v0/item/"+this.hnews[i]+".json?print=pretty").
       subscribe(data => {
           this.hnews_info.push(data);
